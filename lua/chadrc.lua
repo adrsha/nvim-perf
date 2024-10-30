38d8771c2c00df8adf2c93fbdeabc6e37da9ed30
adrsha <adrsha@github.com>
2024-10-30 16:20:43 +0545

    Updates

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,87 +1,91 @@
 local configs = require("lsp_config")
 
 local servers = {
-	html = {},
-	cssls = {},
-	awk_ls = {},
-	bashls = {},
-	rust_analyzer = {},
+  html = {},
+  cssls = {},
+  clangd = {},
+  bashls = {},
+  lua_ls = {},
+  ts_ls = {},
+  rnix_lsp = {},
+  rust_analyzer = {},
+  kotlin_language_server = {},
 
-	pyright = {
-		settings = {
-			python = {
-				analysis = {
-					autoSearchPaths = true,
-					typeCheckingMode = "basic",
-				},
-			},
-		},
-	},
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
+      },
+    },
+  },
 }
 
 for name, opts in pairs(servers) do
-	opts.on_init = configs.on_init
-	opts.on_attach = configs.on_attach
-	opts.capabilities = configs.capabilities
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
 
-	require("lspconfig")[name].setup(opts)
+  require("lspconfig")[name].setup(opts)
 end
 
 local options = {
 
-	base46 = {
-		theme = "everblush", -- default theme
-		hl_add = {},
-		hl_override = {},
-		integrations = {},
-		changed_themes = {},
-		transparency = false,
-		theme_toggle = { "onedark", "one_light" },
-	},
+  base46 = {
+    theme = "everblush", -- default theme
+    hl_add = {},
+    hl_override = {},
+    integrations = {},
+    changed_themes = {},
+    transparency = false,
+    theme_toggle = { "onedark", "one_light" },
+  },
 
-	ui = {
-		cmp = {
-			icons_left = true, -- only for non-atom styles!
-			lspkind_text = true,
-			style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
-			format_colors = {
-				tailwind = true, -- will work for css lsp too
-				icon = "󱓻",
-			},
-		},
-		telescope = { style = "borderless" }, -- borderless / bordered
-		-- lazyload it when there are 1+ buffers
-		tabufline = {
-			enabled = false,
-			lazyload = true,
-			order = { "treeOffset", "buffers", "tabs", "btns" },
-			modules = nil,
-		},
-	},
+  ui = {
+    cmp = {
+      icons_left = true,   -- only for non-atom styles!
+      lspkind_text = true,
+      style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+      format_colors = {
+        tailwind = true,   -- will work for css lsp too
+        icon = "󱓻",
+      },
+    },
+    telescope = { style = "borderless" }, -- borderless / bordered
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+      enabled = false,
+      lazyload = true,
+      order = { "treeOffset", "buffers", "tabs", "btns" },
+      modules = nil,
+    },
+  },
 
-	term = {
-		winopts = { number = false, relativenumber = false },
-		sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-		float = {
-			relative = "editor",
-			row = 0.3,
-			col = 0.1,
-			width = 0.8,
-			height = 0.9,
-			border = "single",
-		},
-	},
+  term = {
+    winopts = { number = false, relativenumber = false },
+    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.1,
+      width = 0.8,
+      height = 0.9,
+      border = "single",
+    },
+  },
 
-	lsp = { signature = true },
+  lsp = { signature = true },
 
-	mason = { pkgs = {} },
+  mason = { pkgs = {} },
 
-	colorify = {
-		enabled = false,
-		mode = "virtual", -- fg, bg, virtual
-		virt_text = "󱓻 ",
-		highlight = { hex = true, lspvars = true },
-	},
+  colorify = {
+    enabled = false,
+    mode = "virtual", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
+  },
 }
 
 local status, chadrc = pcall(require, "chadrc")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,84 +1,91 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter",
 
-	dependencies = {
-		{
-			"nvim-treesitter/nvim-treesitter-context",
-			event = "VeryLazy",
-			dependency = {
-				"nvim-treesitter/nvim-treesitter",
-			},
-		},
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      event = "VeryLazy",
+      dependency = {
+        "nvim-treesitter/nvim-treesitter",
+      },
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      opts = {},
+    },
 
-		{
-			"https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
-			-- event = "VeryLazy",
-			dependency = {
-				"nvim-treesitter/nvim-treesitter",
-			},
-		},
-	},
+    {
+      "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+      -- event = "VeryLazy",
+      dependency = {
+        "nvim-treesitter/nvim-treesitter",
+      },
+    },
+  },
 
-	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-	build = ":TSUpdate",
-	opts = function()
-		pcall(function()
-			dofile(vim.g.base46_cache .. "syntax")
-			dofile(vim.g.base46_cache .. "treesitter")
-		end)
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  build = ":TSUpdate",
+  opts = function()
+    pcall(function()
+      dofile(vim.g.base46_cache .. "syntax")
+      dofile(vim.g.base46_cache .. "treesitter")
+    end)
 
-		return {
-			ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc", "markdown", "regex", "diff" },
+    return {
+      ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc", "markdown", "regex", "diff" },
 
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
 
-			indent = { enable = true },
-		}
-	end,
-	config = function(_, opts)
-		require("treesitter-context").setup({
-			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-			line_numbers = true,
-			multiline_threshold = 20, -- Maximum number of lines to show for a single context
-			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-			mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
-			-- Separator between context and content. Should be a single character string, like '-'.
-			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-			separator = "━",
-			zindex = 21, -- The Z-index of the context window
-			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-		})
+      indent = { enable = true },
+    }
+  end,
+  config = function(_, opts)
+    require("treesitter-context").setup({
+      enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = "outer",     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = "topline",         -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = "━",
+      zindex = 21,     -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    })
 
-		local rainbow_delimiters = require("rainbow-delimiters")
+    local rainbow_delimiters = require("rainbow-delimiters")
 
-		vim.g.rainbow_delimiters = {
-			strategy = {
-				[""] = rainbow_delimiters.strategy["global"],
-			},
-			query = {
-				[""] = "rainbow-delimiters",
-			},
-			priority = {
-				[""] = 110,
-			},
-			highlight = {
-				"RainbowDelimiterRed",
-				"RainbowDelimiterYellow",
-				"RainbowDelimiterBlue",
-				"RainbowDelimiterOrange",
-				"RainbowDelimiterGreen",
-				"RainbowDelimiterViolet",
-				"RainbowDelimiterCyan",
-			},
-			blacklist = { "html" },
-		}
+    vim.g.rainbow_delimiters = {
+      strategy = {
+        [""] = rainbow_delimiters.strategy["global"],
+      },
+      query = {
+        [""] = "rainbow-delimiters",
+      },
+      priority = {
+        [""] = 110,
+      },
+      highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      },
+      blacklist = { "html" },
+    }
 
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
